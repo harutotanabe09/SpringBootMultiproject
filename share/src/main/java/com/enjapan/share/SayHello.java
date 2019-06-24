e13056006b16ccaec7ab14df5d1b1a80091b013c
Haruto Tanabe <harutotanabe@HarutonoMacBook-Air.local>
2019-06-24 22:27:26 +0900

    fisrst

---
+++ share/src/main/java/com/enjapan/share/SayHello.java
@@ -0,0 +1,16 @@
+package com.enjapan.share;
+
+/**
+ * 環境情報
+ */
+public class SayHello {
+
+    /**
+     * OSがWindowsの場合はtrueを返します。
+     *
+     * @return
+     */
+    public String isHello() {
+        return "hello";
+    }
+}
